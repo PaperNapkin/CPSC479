763eaf4ffdde4e211d7d0a4ab3c4570602ed28af
John Orland <jrorland@gmail.com>
2015-04-09 00:09:05 -0400

    menus and stuff

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@
 var ref = new Firebase("https://glaring-heat-449.firebaseio.com");
 var prevMenu = null;
 var currentMenu = null;
+var client = null;
 
 function newUser(){ 
 	ref.createUser({
@@ -39,6 +40,7 @@ function login(){
 
 		  } else { //Successful login
 
+		  	var clientEmail =  $("#input_userName").val();
 		  	$('#authError').addClass("validTxt");
 		  	$('#authError').text("Login Successful!");
 		  	setTimeout(function(){
@@ -46,60 +48,79 @@ function login(){
 		  	}, 2000);
 
                     var s = "https://glaring-heat-449.firebaseio.com/users/" +  authData.uid; //Create the reference to the user list
+                    var s1 = "https://glaring-heat-449.firebaseio.com/map/" ;
                     var usersRef =  new Firebase(s);
                     usersRef.once("value",function(data){
+
                       if (data.val() === null)                                            //If there isn't a record for this user (i.e. first authentication),
                       {                                                                   //Create one and put it on the database
                         console.log("New reference being created...");
-                        usersRef.set({"projects": "", "invites": ""});
+                        usersRef.set({"email": clientEmail, "projects" : "", "invites": ""});
                       }                    
                       else
                       {
                         console.log("user node already exists");
                       }
+
                       console.log("Authenticated successfully with payload:", authData);  //Transition only after we've added the user node to the database
-		      transition("#loginMenu", "#mainMenu", function(){
-		      	$("#title").animate({opacity:0},500, null);
+                      client = authData;
+
+		      		  transition("#loginMenu", "#mainMenu", function(){
+		      				$("#title").animate({opacity:0},500, null);
                       });                    
                       
-		     });
-
-
-                    }); 
+		     	}); 
  		  }
 	});
 }
 
-function newSession(){
+function loadNewSessionMenu(){
 	transition("#mainMenu", "#newSessionMenu", null);
+	var sessionName = $("#input_sessionName").val();
+	var member = $("#input_member").val();
+
+	console.log(member);
 
 	//Name
 	//Collaborator
-}
-
-function oldSession(){
-	transition("#mainMenu", "#oldSessionMenu", null);
 
 }
 
-function previousMenu(){
-	transition(currentMenu, prevMenu, null);
+function loadGalleryMenu(){
+	
+	var loadGalleryData = function () {
+		// Query firebase for gallery related data
+		// make sure the transition call below is called after data retrieval completes.
+		transition("#loading", "#galleryMenu", null);
+	}
+
+	transition("#mainMenu", "#loading", loadGalleryData);
+
+}
+
+function loadMainMenu(){
+	transition(currentMenu, "#mainMenu", null);
 }
 
 function beginTurn(){
-	console.log("lol!");
+	;
 }
 
 function transition(last, next, callback){
 	prevMenu = last;
 	currentMenu = next;
+	console.log(last, next);
+	
 	var afterAnim = function(){
 		$(last).hide();
+		console.log(next);
 		$(next).show();
-		$(next).animate({opacity:1},500,callback);
+		$(next).animate( {opacity:1}, 500, callback);
 		$("button").removeAttr('disabled');
 	}
+
 	$(last).animate({opacity:0},500,afterAnim);
 	$("button").attr('disabled', "true");
 }
+
 
